1e04955964b7cebb522768da5060b707a3224727
Vitor Hugo <vitorhugoemail@gmail.com>
2024-09-02 17:55:19 -0300

    Fear: Estrutura inicial

---
+++ src/main/java/com/ecomeecer/ecomecercer/EcomecercerApplication.java
@@ -0,0 +1,13 @@
+package com.ecomeecer.ecomecercer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomecercerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomecercerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomeecer/ecomecercer/EcomecercerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomeecer.ecomecercer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomecercerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
